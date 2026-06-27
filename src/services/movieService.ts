d2318bbb43ca423ff7b93b2f34e86f59cb7b39d8
volodymyr <zhurakivskyivolodymyr@gmail.com>
2026-06-27 11:16:01 +0300

    fix-інтерфейси для відповіді API

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
-import { type TMDBResponse } from '../types/movie';
+import type { Movie } from '../types/movie'; // Залишаємо імпорт лише для поодинокого фільму
+
+// 1. Переносимо інтерфейс відповіді API сюди, як вимагає ментор
+export interface TMDBResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
@@ -8,7 +16,7 @@ const api = axios.create({
 export const fetchMovies = async (query: string, page: number): Promise<TMDBResponse> => {
   const response = await api.get<TMDBResponse>('/search/movie', {
     params: {
-      api_key: 'e932e66ee5819c11bae66b66cde73bfb', // Твій робочий ключ
+      api_key: 'e932e66ee5819c11bae66b66cde73bfb',
       query,
       page,
       include_adult: false,
--- src/types/movie.ts
+++ src/types/movie.ts
@@ -7,10 +7,3 @@ export interface Movie {
   backdrop_path: string | null;
   vote_average: number;
 }
-
-export interface TMDBResponse {
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-  page: number;
-}
